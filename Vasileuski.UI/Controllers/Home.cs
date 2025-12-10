39e43e1ecf93cd4daf4a579b0931dd52bcf1313d
EvgeniiVasileuski <vasil93@internet.ru>
2025-12-10 12:47:40 +0300

    pic8

---
+++ Vasileuski.UI/Controllers/Home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vasileuski.UI.Controllers
+{
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
