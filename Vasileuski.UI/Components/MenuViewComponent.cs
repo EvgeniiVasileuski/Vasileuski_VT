40ec856f80947ab878fc711a352dbea6592d7d9a
EvgeniiVasileuski <vasil93@internet.ru>
2025-12-12 12:13:03 +0300

    lab2task3.6

---
+++ Vasileuski.UI/Components/MenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Vasileuski.UI.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            // Получаем имя текущего контроллера и области
+            var controller = Request.RouteValues["controller"]?.ToString()?.ToLower() ?? string.Empty;
+            var area = Request.RouteValues["area"]?.ToString()?.ToLower() ?? string.Empty;
+
+            // Передаем данные в представление
+            ViewData["Controller"] = controller;
+            ViewData["Area"] = area;
+
+            // Также можно передать дополнительные данные, если нужно
+            // Например, список пунктов меню из БД
+
+            return View();
+        }
+    }
+
+}
